5daa45637269fde9156bc61a80485a8ca97a2059
Ivan Delaš <ivandelas@gmail.com>
2018-01-10 00:13:13 +0100

    done

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,12 @@
-require 'pry'
+# require 'pry'
+collection = []
 
 def my_find(collection)
+  num = 0
+
+  while num < collection.length
+    return collection[num] if yield collection[num]
+    num += 1
+  end
 
 end
